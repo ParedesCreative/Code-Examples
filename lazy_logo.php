1c3fc1e85ec94cac14a503fb3667715f296c3746
Victor Paredes <victor@paredescreative.com>
2021-12-31 16:42:47 -0800

    Adding conditional for local/production environments

--- lazy_logo.php
+++ lazy_logo.php
@@ -1,12 +1,28 @@
 <?php
 
-return [
+if (config('app.env') === 'local') {
 
-    'text'        => env('APP_NAME', 'Laravel'),
-    'color'       => env('LAZY_LOGO_COLOR', '#19292f'), // Use an array for a gradient: ['#f94f57', '#c471ed', '#12c2e9']
-    'font_family' => 'Inter UI,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue',
-    'font_size'   => '20px',
-    'font_weight' => '500',
-    'font_shift'  => '1',
+    return [
+        // LOCAL
+        'text' => env('APP_NAME', 'Laravel'),
+        'color' => env('LAZY_LOGO_COLOR', '#19292f'), // Use an array for a gradient: ['#f94f57', '#c471ed', '#12c2e9']
+        'font_family' => 'Inter UI,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue',
+        'font_size' => '20px',
+        'font_weight' => '500',
+        'font_shift' => '1',
 
-];
+    ];
+
+} else {
+
+    return [
+        // PRODUCTION
+        'text'        => env('APP_NAME', 'Laravel'),
+        'color' => env('LAZY_LOGO_COLOR', '#ff0000'), // Use an array for a gradient: ['#f94f57', '#c471ed', '#12c2e9']
+        'font_family' => 'Inter UI,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue',
+        'font_size' => '30px',
+        'font_weight' => '500',
+        'font_shift' => '1',
+
+    ];
+}
